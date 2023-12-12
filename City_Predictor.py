411b13eb2be876b1153f5f4790642ea695fe5136
unknown <mrayagers@gmail.com>
2023-12-11 23:29:49 -0600

    Added city predictor shell with url builder

---
+++ City_Predictor.py
@@ -0,0 +1,41 @@
+from urllib.parse import urlencode
+from sign_url import sign_url
+try:
+	from config import api_key
+	from config import secret
+except:
+	print('You must provide your own api_key and secret in a file named config.py\nPlease reference sample_config.py to view the correct format')
+	raise
+	
+#https://maps.googleapis.com/maps/api/streetview?parameters
+#parameters = [location, size, key, heading, fov, pitch, radius]
+#location - a latitude and longitude coordinates pair ex: (40.457375,-80.009353)
+#size - output size of the image in pixels. Size is specified as {width}x{height} [0,640]x[0,640]
+#key - API Key
+#heading - indicates the compass heading of the camera [0,360] 0=North
+#fov - determines the horizontal field of view of the image [0-120]
+	#When dealing with a Street View image of a set size, field of view in essence represents zoom, with smaller numbers indicating a higher level of zoom.
+#pitch - specifies the up or down angle of the camera relative to the Street View vehicle [-90,90] 0=flat
+#radius - sets a radius, specified in meters, in which to search for a panorama [0,inf)
+def input_url_builder(location,size,key,heading=None,fov=None,pitch=None,radius=None):
+	GMAPS_URL = 'https://maps.googleapis.com/maps/api/streetview?'
+	
+	params = {
+		'location': f'{location[0]},{location[1]}',
+		'size': f'{size[0]}x{size[1]}',
+		'key': key,
+	}
+	
+	if heading is not None:
+		params['heading'] = heading
+		
+	if fov is not None:
+		params['fov'] = fov
+		
+	if pitch is not None:
+		params['pitch'] = pitch
+		
+	if radius is not None:
+		params['radius'] = radius
+	
+	return GMAPS_URL + urlencode(params)
